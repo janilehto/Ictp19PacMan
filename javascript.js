f48c42f36dfc01a271af2077a76ecd4da48dec87
Jani Lehto <jani.lehto@careeria.fi>
2020-12-04 13:00:18 +0200

    Tyhjän solun tarkistus lisätty

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 //Pacmanin sijainti
-var posRow=2, posCel=1;
+var posRow=1, posCel=1;
 const maxRow=2, maxCel=2;
 function movePacMan(){
 	document.getElementById("playground").rows[posRow].cells[posCel].innerHTML='<img src="./kuvat/pacman.png">';
@@ -8,23 +8,44 @@ function clearCell(){
 	document.getElementById("playground").rows[posRow].cells[posCel].innerHTML='';
 }
 
-function moveUp(){
-	clearCell();
-	if( posRow == 0 ){
-		posRow = maxRow;
-	}else{
-		posRow--;
+function isCellEmpty(r, c){
+	var paikka = document.getElementById("playground").rows[r].cells[c].innerHTML;
+	if( paikka.indexOf("brick.jpg") == -1){
+		posRow = r;
+		posCel = c;
 	}
-	movePacMan();
+}
+
+function moveUp(){
+	var x = posRow;
+	if( x == 0 ){
+		x = maxRow;
+	}else{
+		x--;
+	}
+	isCellEmpty(x, posCel);
+}
+function moveDown(){
+	var x = posRow;
+	if( x == maxRow ){
+		x = 0;
+	}else{
+		x++;
+	}
+	isCellEmpty(x, posCel);
 }
 
 function run(){
 	document.addEventListener('keydown', 
 		function(event) {
+			clearCell();
 			switch( event.code ){
 				case "KeyW": moveUp();
 				break;
+				case "KeyS": moveDown();
+				break;			
 			}
+			movePacMan();
 			console.log(event.code);
 		}
 	);
